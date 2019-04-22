acedc88d6f515fb578ed5f03a85fb89984009201
Norhan Khaled <norhan.ali.araba@ibm.com>
2019-04-22 22:09:25 +0200

    changes in EX3

--- NodeApp/Ex3/services/articleServices.js
+++ NodeApp/Ex3/services/articleServices.js
@@ -4,7 +4,7 @@ let NaturalLanguageUnderstandingV1 =
 require('watson-developer-cloud/natural-language-understanding/v1.js');
 let natural_language_understanding = new NaturalLanguageUnderstandingV1({
   version: '2018-11-16',
-iam_apikey: 'SET_YOUR_OWN_API_KEY'
+  iam_apikey: SET_YOUR_APIKEY,
 url: 'https://gateway-lon.watsonplatform.net/natural-language-understanding/api'
 }); 
 //error message for missing URL
@@ -14,7 +14,12 @@ const MISSING_URL_ERROR = 'URL not passed';
 /*
  * Call Watson NLU Service to extract the list of author names for the requested article URL 
 */
-exports.extractArticleAuthorNames = function(req){
+exports.extractArticleAuthorNames = function(req , callback){
+	//If the url is not passed, return error to the caller
+	if(req===null||req.body===null||req.body.url===null){
+		callback(MISSING_URL_ERROR,null);
+		return;
+	}
 	// url is the parameter passed in the POST request to /author
 // It contains the URL of the article
 // The metadata feature returns the author, title, and publication date. 
@@ -33,11 +38,7 @@ exports.extractArticleAuthorNames = function(req){
             callback(null,response.metadata.authors);
     });
 
-//If the url is not passed, return error to the caller
-	if(req===null||req.body===null||req.body.url===null){
-		callback(MISSING_URL_ERROR,null);
-		return;
-	}
+
 
 
 
